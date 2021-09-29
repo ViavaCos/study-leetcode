ec85f857d1155c9b5850e3dc99340f7d80cb09e5
ViavaCos <13811092694@163.com>
2021-09-29 00:46:02 +0000

    剑指 Offer 03. 数组中重复的数字

---
+++ 简单/剑指 Offer 03. 数组中重复的数字.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findRepeatNumber = function(nums) {
+    nums = nums.sort((a, b) => a - b)
+
+    for(let i = 1; i<nums.length; i++) {
+        if(nums[i] == nums[i - 1]){
+            return nums[i]
+        }
+    }
+};
